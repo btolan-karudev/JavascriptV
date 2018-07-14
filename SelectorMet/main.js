b6801c60ef2da4910c16498248b1c06928950465
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2018-07-14 23:26:29 +0200

    querry selectors

---
+++ SelectorMet/main.js
@@ -0,0 +1,6 @@
+var tag = document.getElementById("lumiere");
+
+var tags = document.getElementsByClassName("gras");
+
+
+//querrySELECTOR for one item ot qSElAll for multiple items REMPLACE all the others but we need to specifie the dot or the id sign
--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 var qselec = document.querySelector("h1");
 
-qselec.style.color = "red";
+qselec.style.color = "pink";
 
 console.log(qselec);
+
+var body = document.querySelector("body");
+var isBlue = false;
+
+setInterval(function(){
+    if (isBlue) {
+        body.style.background = "white";
+    } else {
+        body.style.background = "#3498db";
+    }
+    isBlue = !isBlue;
+}, 1000)
